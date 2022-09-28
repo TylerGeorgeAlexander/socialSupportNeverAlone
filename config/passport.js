cbdeb90b08cf203a0bea448c4ca8fd0931a2507d
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-09-27 18:35:25 -0700

    Passport config fix / workaround

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,77 @@
-const LocalStrategy = require('passport-local').Strategy
-const mongoose = require('mongoose')
-const User = require('../models/User')
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const User = require("../models/User");
+const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
+const passport = require("passport");
+const refresh = require("passport-oauth2-refresh");
 
-module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+// Local
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
     User.findOne({ email: email.toLowerCase() }, (err, user) => {
-      if (err) { return done(err) }
+      if (err) {
+        return done(err);
+      }
       if (!user) {
-        return done(null, false, { msg: `Email ${email} not found.` })
+        return done(null, false, { msg: `Email ${email} not found.` });
       }
       if (!user.password) {
-        return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
+        return done(null, false, {
+          msg: "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
+        });
       }
       user.comparePassword(password, (err, isMatch) => {
-        if (err) { return done(err) }
-        if (isMatch) {
-          return done(null, user)
+        if (err) {
+          return done(err);
         }
-        return done(null, false, { msg: 'Invalid email or password.' })
-      })
-    })
-  }))
-  
-
-  passport.serializeUser((user, done) => {
-    done(null, user.id)
+        if (isMatch) {
+          return done(null, user);
+        }
+        return done(null, false, { msg: "Invalid email or password." });
+      });
+    });
   })
+);
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
-  })
-}
+// Google
+const googleStrategyConfig = new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const newUser = {
+        google: profile.id,
+        profile: {
+          name: `${profile.name.familyName + " " + profile.name.givenName}`,
+          picture: profile.photos[0].value,
+        },
+      };
+
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (user) {
+          done(null, user);
+        } else {
+          user = await User.create(newUser);
+          done(null, user);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  )
+
+
+passport.use("google", googleStrategyConfig);
+refresh.use("google", googleStrategyConfig);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => done(err, user));
+});
